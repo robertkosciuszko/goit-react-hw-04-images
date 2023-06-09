1bf0fc36ff90833cbfd78e21e2b2bf8c2d972dc7
robertkosciuszko <114183493+robertkosciuszko@users.noreply.github.com>
2023-06-09 17:23:01 +0200

    uppdate

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,22 @@
+import propTypes from 'prop-types';
+import css from './ImageGalleryItem.module.css';
+
+export const ImageGalleryItem = ({ image, onclick }) => (
+  <li
+    className={css.ImageGalleryItem}
+    id={image.id}
+    onClick={() => onclick(image.largeImageURL, image.tags)}
+  >
+    <img
+      src={image.webformatURL}
+      alt={image.tags}
+      name={image.largeImageURL}
+      className={css.ImageGalleryItemImage}
+    />
+  </li>
+);
+
+ImageGalleryItem.propTypes = {
+  image: propTypes.object.isRequired,
+  onclick: propTypes.func.isRequired,
+};
